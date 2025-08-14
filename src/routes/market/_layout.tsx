8f2a6b85685cb0d04f371c48ad735e4a1dc107f2
dk-okonkwo <dokonkwo19@gmail.com>
2025-08-14 17:52:47 +0200

    deleted old files, commented out unused variables

--- src/routes/market/_layout.tsx
+++ src/routes/market/_layout.tsx
@@ -1,15 +1,15 @@
-import { createFileRoute, Outlet, useMatchRoute } from "@tanstack/react-router";
-import ResultsOptions from "@/components/resultsOptions";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+// import ResultsOptions from "@/components/resultsOptions";
 export const Route = createFileRoute("/market/_layout")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const matchRoute = useMatchRoute();
-  let currentSubRoute: string = "";
-  if (matchRoute({ to: "/market/product" })) currentSubRoute = "Product";
-  if (matchRoute({ to: "/market/service" })) currentSubRoute = "Service";
-  if (matchRoute({ to: "/market/taka" })) currentSubRoute = "Taka";
+  // const matchRoute = useMatchRoute();
+  // let currentSubRoute: string = "";
+  // if (matchRoute({ to: "/market/product" })) currentSubRoute = "Product";
+  // if (matchRoute({ to: "/market/service" })) currentSubRoute = "Service";
+  // if (matchRoute({ to: "/market/taka" })) currentSubRoute = "Taka";
 
   return (
     <div className="">
