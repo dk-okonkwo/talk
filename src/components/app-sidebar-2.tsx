e5d72eb79d4714c22f6e94804e142bd79d32f5ee
dk-okonkwo <dokonkwo19@gmail.com>
2025-08-15 11:57:08 +0200

    more unused imports removed

--- src/components/app-sidebar-2.tsx
+++ src/components/app-sidebar-2.tsx
@@ -12,7 +12,6 @@ import {
   SidebarGroupContent,
   SidebarHeader,
   SidebarInput,
-  useSidebar,
 } from "@/components/ui/sidebar";
 import { Switch } from "@/components/ui/switch";
 
--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import * as React from 'react'
-import { ArrowUpIcon, CheckIcon, PlusIcon } from 'lucide-react'
+import { CheckIcon, PlusIcon } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
